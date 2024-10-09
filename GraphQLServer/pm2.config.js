636cd0e5d2891155f72cb0aec34d58dff2539ec4
Juan <troindx@gmail.com>
2024-10-09 16:34:09 +0200

    added pm2 config file to graphql server

---
+++ GraphQLServer/pm2.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+      {
+        name: "apollo-server",
+        script: "./dist/index.js", // Relative path to the compiled JavaScript file
+        instances: 1, // Number of instances to run (can be 'max' for all cores)
+        exec_mode: "fork", // Use fork mode
+        cwd: "/home/troin/apollo-server",
+        env: {
+          NODE_ENV: "production", // Set environment to production
+        },
+        interpreter: "node", // Use Node.js to execute the script
+        cwd: "./", // Working directory for your app (set it to your project root)
+        error_file: "./logs/err.log", // Path to error logs (relative)
+        out_file: "./logs/out.log", // Path to output logs (relative)
+        log_date_format: "YYYY-MM-DD HH:mm Z", // Log date format
+      },
+    ],
+  };
