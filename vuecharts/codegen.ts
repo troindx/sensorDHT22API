59bba880af10e207feef648713265664d933fbf6
Juan <troindx@gmail.com>
2024-10-09 19:00:22 +0200

    changed codegen typescript file

--- vuecharts/codegen.ts
+++ vuecharts/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'https://hamrodev.com:4000',
+  schema: 'https://hamrodev.com/graphql',
   documents: 'src/**/*.vue',
   generates: {
     'src/gql/': {
